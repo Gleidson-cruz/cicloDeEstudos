897d74ed35621b5480610fd6f0b2960ac867205f
Gleidson-cruz <gleidsontech@gmail.com>
2024-12-04 10:27:37 -0300

    Validação do formulário

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,80 @@
+let nomeMateria = null;
+let pesoMateria = null;
+const cargaHoraria = null;
+
+
+
+
 const form = document.getElementById("myform");
 
-function validarMat(input1) {
-    if (input1.trim()  === "") {
-        return "Todos os campos são obrigatórios";
-    }
-
-    if (!input1.match(/^[a-zA-Z0-9]+$/)) {
-        return "Por favor, insira os dados corretamente. <br> O campo 'Matéria' aceita letras e números. <br> O campo 'Peso' aceita apenas numeros";
-    }
-
-    return "";
+function validarCarga(carga) {
+  if (carga.trim() === "") {
+    return (document.getElementById("alertCarga").innerHTML =
+      "Esse campo é obrigatório");
+  }
+  if (!carga.match(/^[0-9]+$/)) {
+    return (document.getElementById("alertCarga").innerHTML =
+      "Apenas caracteres numéricos são aceitos");
+  }
+  if (parseInt(carga) > 24) {
+    return (document.getElementById("alertPeso").innerHTML =
+      "A carga horária não pode ser maior que 24");
+  }
 }
 
-function validarPeso(input2) {
-    if (input2.trim()  === "") {
-        return "Todos os campos são obrigatórios";
-    }
-
-    if (!input2.match(/^[0-9]+$/)) {
-        return "Por favor, insira o dado corretamente";
-    }
-
-    return "";
+function validarMat(materia) {
+  if (materia.trim() === "") {
+    return (document.getElementById("alertMat").innerHTML =
+      "Esse campo é obrigatório");
+  } else if (!materia.match(/^[a-zA-Z0-9]+$/)) {
+    return (document.getElementById("alertPeso").innerHTML =
+      "Apenas letras(a-z,A-Z) e numeros(0-9) são aceitos");
+  }
 }
 
+function validarPeso(peso) {
+  if (peso.trim() === "") {
+    return (document.getElementById("alertPeso").innerHTML =
+      "Esse campo é obrigatório");
+  }
+  if (!peso.match(/^[0-9]+$/)) {
+    return (document.getElementById("alertPeso").innerHTML =
+      "Apenas caracteres numéricos são aceitos");
+  }
+}
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    let userPeso = document.getElementById("insertPeso").value
-    let matUser = document.getElementById("insertInfo").value;
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let userPeso = document.getElementById("insertPeso").value;
+  let matUser = document.getElementById("insertInfo").value;
+  let matCarga = document.getElementById("insertCarga").value;
 
+  const errMsgMat = validarMat(matUser);
+  const errMsgPeso = validarPeso(userPeso);
+  const errMsgCarga = validarCarga(matCarga);
 
-    const errMsgMat = validarMat(matUser);
-    const errMsgPeso = validarPeso(userPeso);
+  document.getElementById("passalerta").innerHTML = "";
+  document.getElementById("alertPeso").innerHTML = "";
+  document.getElementById("alertMat").innerHTML = "";
+  document.getElementById("alertCarga").innerHTML = "";
 
-    document.getElementById("passalerta").innerHTML = "";
-
-
-    if (errMsgPeso) {
-        document.getElementById("passalerta").style.color = "#ff0000"
-        document.getElementById("passalerta").innerHTML = errMsgPeso;
-    }
-
-    if (errMsgMat) {
-        document.getElementById("passalerta").style.color = "#ff0000"
-        document.getElementById("passalerta").innerHTML = errMsgMat;
-    }
-
+  if (errMsgPeso) {
+    document.getElementById("alertPeso").style.color = "#ff0000";
+    document.getElementById("alertPeso").innerHTML = errMsgPeso;
+  }
+  if (errMsgMat) {
+    document.getElementById("alertMat").style.color = "#ff0000";
+    document.getElementById("alertMat").innerHTML = errMsgMat;
+  }
+  if (errMsgCarga) {
+    document.getElementById("alertCarga").style.color = "#ff0000";
+    document.getElementById("alertCarga").innerHTML = errMsgCarga;
+  }
+  if (!errMsgCarga) {
     if (!errMsgPeso && !errMsgMat) {
-        document.getElementById("passalerta").innerHTML = "Materia adcionada com sucesso";
-        document.getElementById("passalerta").style.color = "#64b46f"
+      document.getElementById("passalerta").innerHTML =
+        "Materia adcionada com sucesso";
+      document.getElementById("passalerta").style.color = "#64b46f";
     }
-
+  }
 });
