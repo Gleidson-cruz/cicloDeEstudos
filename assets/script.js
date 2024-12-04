663851ee2711ba3782806e23a4316d50f3e7bb7a
Gleidson-cruz <gleidsontech@gmail.com>
2024-12-04 13:53:40 -0300

    Adição de campos no fomulário e ajustes

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,43 @@
 let nomeMateria = null;
 let pesoMateria = null;
-const cargaHoraria = null;
+let cargaHorariaDia = null;
+let cargaHorariaSemana = null;
+let cargaHorariaTotal = null;
+let somaDificuldade = [];
 
+const criarCiclo = document.getElementById("criar");
 
-
+// inicio Validação do formulário
 
 const form = document.getElementById("myform");
 
-function validarCarga(carga) {
-  if (carga.trim() === "") {
-    return (document.getElementById("alertCarga").innerHTML =
+function validarCarga(cargaDia) {
+  if (cargaDia.trim() === "") {
+    return (document.getElementById("alertCargaDia").innerHTML =
       "Esse campo é obrigatório");
   }
-  if (!carga.match(/^[0-9]+$/)) {
-    return (document.getElementById("alertCarga").innerHTML =
+  if (!cargaDia.match(/^[0-9]+$/)) {
+    return (document.getElementById("alertCargaDia").innerHTML =
       "Apenas caracteres numéricos são aceitos");
   }
-  if (parseInt(carga) > 24) {
-    return (document.getElementById("alertPeso").innerHTML =
-      "A carga horária não pode ser maior que 24");
+  if (parseInt(cargaDia) > 24) {
+    return (document.getElementById("alertCargaDia").innerHTML =
+      "A carga horária do dia não pode ser maior que 24");
+  }
+}
+
+function validarCargaSemana(cargaSemana) {
+  if (cargaSemana.trim() === "") {
+    return (document.getElementById("alertCargaSemana").innerHTML =
+      "Esse campo é obrigatório");
+  }
+  if (!cargaSemana.match(/^[0-9]+$/)) {
+    return (document.getElementById("alertCargaSemana").innerHTML =
+      "Apenas caracteres numéricos são aceitos");
+  }
+  if (parseInt(cargaSemana) > 7) {
+    return (document.getElementById("alertCargaSemana").innerHTML =
+      "A carga horária da semana não pode ser maior que 7");
   }
 }
 
@@ -26,8 +45,8 @@ function validarMat(materia) {
   if (materia.trim() === "") {
     return (document.getElementById("alertMat").innerHTML =
       "Esse campo é obrigatório");
-  } else if (!materia.match(/^[a-zA-Z0-9]+$/)) {
-    return (document.getElementById("alertPeso").innerHTML =
+  } else if (!materia.match(/^[a-zA-Z0-9\s]+$/)) {
+    return (document.getElementById("alertMat").innerHTML =
       "Apenas letras(a-z,A-Z) e numeros(0-9) são aceitos");
   }
 }
@@ -41,22 +60,34 @@ function validarPeso(peso) {
     return (document.getElementById("alertPeso").innerHTML =
       "Apenas caracteres numéricos são aceitos");
   }
+  if (parseInt(peso) > 5) {
+    return (document.getElementById("alertCargaSemana").innerHTML =
+      "O grau de dificuldade não pode ser maior que 5");
+  }
+}
+
+function limparMsg() {
+    document.getElementById("passalerta").innerHTML = "";
+    document.getElementById("alertPeso").innerHTML = "";
+    document.getElementById("alertMat").innerHTML = "";
+    document.getElementById("alertCargaDia").innerHTML = "";
+    document.getElementById("alertCargaSemana").innerHTML = "";
 }
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+
+limparMsg()
+
   let userPeso = document.getElementById("insertPeso").value;
-  let matUser = document.getElementById("insertInfo").value;
-  let matCarga = document.getElementById("insertCarga").value;
+  let matUser = document.getElementById("insertMat").value;
+  let matCargaDia = document.getElementById("insertCargaDia").value;
+  let matCargaSemana = document.getElementById("insertCargaSemana").value;
 
   const errMsgMat = validarMat(matUser);
   const errMsgPeso = validarPeso(userPeso);
-  const errMsgCarga = validarCarga(matCarga);
-
-  document.getElementById("passalerta").innerHTML = "";
-  document.getElementById("alertPeso").innerHTML = "";
-  document.getElementById("alertMat").innerHTML = "";
-  document.getElementById("alertCarga").innerHTML = "";
+  const errMsgCargaDia = validarCarga(matCargaDia);
+  const errMsgCargaSemana = validarCargaSemana(matCargaSemana);
 
   if (errMsgPeso) {
     document.getElementById("alertPeso").style.color = "#ff0000";
@@ -66,15 +97,39 @@ form.addEventListener("submit", function (event) {
     document.getElementById("alertMat").style.color = "#ff0000";
     document.getElementById("alertMat").innerHTML = errMsgMat;
   }
-  if (errMsgCarga) {
-    document.getElementById("alertCarga").style.color = "#ff0000";
-    document.getElementById("alertCarga").innerHTML = errMsgCarga;
+  if (errMsgCargaDia) {
+    document.getElementById("alertCargaDia").style.color = "#ff0000";
+    document.getElementById("alertCargaDia").innerHTML = errMsgCargaDia;
   }
-  if (!errMsgCarga) {
-    if (!errMsgPeso && !errMsgMat) {
-      document.getElementById("passalerta").innerHTML =
-        "Materia adcionada com sucesso";
-      document.getElementById("passalerta").style.color = "#64b46f";
-    }
+  if (errMsgCargaSemana) {
+    document.getElementById("alertCargaSemana").style.color = "#ff0000";
+    document.getElementById("alertCargaSemana").innerHTML = errMsgCargaSemana;
+  }
+  if (!errMsgPeso && !errMsgMat && !errMsgCargaDia && !errMsgCargaSemana) {
+    cargaHorariaDia = parseInt(matCargaDia);
+    cargaHorariaSemana = parseInt(matCargaSemana);
+    nomeMateria = matUser;
+    pesoMateria = userPeso;
+    cargaHorariaTotal = cargaHorariaDia * cargaHorariaSemana;
+
+    document.getElementById("passalerta").innerHTML =
+      "Matéria adicionada com sucesso";
+    document.getElementById("passalerta").style.color = "#64b46f";
+
+    somaDificuldade.push(parseInt(userPeso))
+    console.log(somaDificuldade)
+    form.reset();
   }
 });
+
+// Fim Validação do formulário
+
+function somaPeso() {
+    let soma = 0;
+    for (let i=0; i < somaDificuldade.length; i++) {
+        soma += somaDificuldade[i];
+    }
+    
+    console.log(somaDificuldade)
+    console.log(soma);
+}
